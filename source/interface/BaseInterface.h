8a839e2e16529bd91ea87fe2f53991d311c844fc
Farley Runkel <farleyrunkel@gmail.com>
2024-09-16 23:53:52 +0800

    ✨ feat: Add SARibbonBar and QSplitter to CoreWidget

--- source/interface/BaseInterface.h
+++ source/interface/BaseInterface.h
@@ -1,12 +1,15 @@
 #ifndef BASE_INTERFACE_H
 #define BASE_INTERFACE_H
 
+#include <QString>
 
 class BaseInterface {
-
-
-
+public:
+    virtual ~BaseInterface() {}
+    virtual QString name() const = 0;
 };
 
 
-#endif // !BASE_INTERFACE_H
+#endif // BASE_INTERFACE_H
+
+Q_DECLARE_INTERFACE(BaseInterface, "TinyBlender.BaseInterface")
--- source/primitives/primitivesGenerator.h
+++ source/primitives/primitivesGenerator.h
@@ -1,16 +1,26 @@
 #ifndef PRIMITIVESGENERATOR_H
 #define PRIMITIVESGENERATOR_H
 
-#include <QObejct>
+#include <QObject>
+#include <QString>
+#include <QPluginLoader>
 
 #include "baseinterface.h"
 
 
-class PrimitivesGenerator : public QObject, public BasePlugin {
+class PrimitivesGenerator : public QObject, public BaseInterface {
+	Q_OBJECT
+	Q_INTERFACES(BaseInterface)
+	Q_PLUGIN_METADATA(IID "TinyBlender.Plugins.PrimitiveGenerator")
 
+public:
+	QString name() const { 
+		return QString("PrimitivesGenerator"); 
+	};
 
 
 };
 
-
 #endif // !PRIMITIVESGENERATOR_H
+
+// Q_IMPORT_PLUGIN(PrimitivesGenerator)
